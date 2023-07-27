45104b116313d78c5021252859ccf696e9988c04
Robin Torres <robinhood9607@gmail.com>
2023-07-27 16:04:55 -0600

    tercer commit app garoo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
-import App from "./App.js"
+import App from "./App"
 
 
 const app = document.getElementById("app")
