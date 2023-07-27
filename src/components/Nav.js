c6314274c97828f3bfd7580c32085cd04d06e7ec
Robin Torres <robinhood9607@gmail.com>
2023-07-27 16:06:16 -0600

    cuarto commit app garoo

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Nav() {
+    return <>
+   <ul>
+   <li><link to="/">Home</link></li>
+   <li><link to="/about">About</link></li>
+
+   </ul>
+        </>
+}
+export default Nav
