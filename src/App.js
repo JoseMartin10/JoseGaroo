fbe4bc635fc6efbbc3909fceb2ad66db3166fd35
Robin Torres <robinhood9607@gmail.com>
2023-08-01 09:21:40 -0600

    commit para cambiarme de rama

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from "./components/Sidebar"
 import Footer from "./components/Footer"
 import Nav from "./components/Nav"
 import Layout from "./layaout/layout"
+import Clients from "./pages/clients"
 
 function App() {
     return <>
@@ -12,6 +13,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/" element={<Sidebar />}/>
+        <Route exact path="client" element={<Clients />}/>
         <Route path="footer" element={<Footer />}/>
         <Route path="*" element={<Home />}/>
         </Routes>
